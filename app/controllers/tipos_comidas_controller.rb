581a4b4f0a6695e7ed776e851090c2975768413d
malborno2020 <manuel.albornoz@zoho.com>
2021-10-16 14:17:11 -0300

    creando nuevos controladores

--- app/controllers/tipos_comidas_controller.rb
+++ app/controllers/tipos_comidas_controller.rb
@@ -1,8 +1,32 @@
-class TiposComidasController <ApplicationController
+class TiposComidasController < ApplicationController
 
-    #GET /tipos comidas
-
+    # GET /tipos_comidas
     def listar
         @todos_los_tipos = TipoComida.all
+        @titulo_link = "Mensaje desde el contralor"
+    end
+
+    # GET /nuevo_tipo_comida
+    def nuevo
+        @nuevo_tipo_comida = TipoComida.new
+    end
+
+    # POST /nuevo_tipo_comida
+    def crear
+        # guardar lo que llegue del formulario en la base de datos
+        datos_tipo_comida = params.require(:tipo_comida).permit(:tipo)
+        nuevo_tipo = TipoComida.new(datos_tipo_comida)
+        nuevo_tipo.save
+        redirect_to listar_tipos_comidas_path
+    end
+
+    # DELETE tipos_comidas/:id
+    def eliminar
+        # pasos para eliminar un registro
+        # 1. buscar el registro por ID
+        tipo = TipoComida.find(params[:id])
+        # 2. Intentar eliminar el registro
+        tipo.destroy
+        redirect_to listar_tipos_comidas_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'tipos_comidas', to: 'tipos_comidas#listar'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Rutas Tipos Comidas
+  get      'tipos_comidas',       to: 'tipos_comidas#listar',     as: 'listar_tipos_comidas'
+  get      'tipos_comidas/nuevo', to: 'tipos_comidas#nuevo',      as: 'nuevo_tipo_comida'
+
+  post     'nuevo_tipo_comida',   to: 'tipos_comidas#crear'
+  
+  delete   'tipos_comidas/:id',   to: 'tipos_comidas#eliminar',   as: 'eliminar_tipo_comida'  
 end
