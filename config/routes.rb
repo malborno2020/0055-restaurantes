8a838ade2e1c21bf863f1215b9ce4c2d08fb2abe
malborno2020 <manuel.albornoz@zoho.com>
2021-10-20 19:44:15 -0300

    agregando rutas de puntajes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,22 @@ Rails.application.routes.draw do
   # Rutas Tipos Comidas
   get      'tipos_comidas',             to: 'tipos_comidas#listar',     as: 'tipos_comidas'     # listar 
   get      'tipos_comidas/nuevo',       to: 'tipos_comidas#crear',      as: 'nuevo_tipo_comida' # formulario de nuevo
-  get        'tipos_comidas/:id',         to: 'tipos_comidas#mostrar',    as: 'tipo_comida'       # vista del detalle de un tipo de comida
+  get      'tipos_comidas/:id',         to: 'tipos_comidas#mostrar',    as: 'tipo_comida'       # vista del detalle de un tipo de comida
   get      'tipos_comidas/:id/editar',  to: 'tipos_comidas#editar',     as: 'editar_tipo_comida'# formulario para editar el registro
 
   post     'tipos_comidas',             to: 'tipos_comidas#guardar'
+  put      'tipos_comidas/:id',         to: 'tipos_comidas#actualizar'
+  patch    'tipos_comidas/:id',         to: 'tipos_comidas#actualizar'
   delete   'tipos_comidas/:id',         to: 'tipos_comidas#eliminar'
 
+  # Rutas Puntajes
+  get       'puntajes',                 to: 'puntajes#listar',          as: 'puntajes'
+  get       'puntajes/nuevo',           to: 'puntajes#crear',           as: 'nuevo_puntaje'
+  get       'puntajes/:id',             to: 'puntajes#mostrar',         as: 'puntaje'
+  get       'puntajes/:id/editar',      to: 'puntajes#editar',          as: 'editar_puntaje'
+
+  post      'puntajes',                 to: 'puntajes#guardar'
+  put       'puntajes/:id',             to: 'puntajes#actualizar'
+  patch     'puntajes/:id',             to: 'puntajes#actualizar'
+  delete    'puntajes/:id',             to: 'puntajes#eliminar'
 end
