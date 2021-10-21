0d39a15ef702d810a8e65633c1216fd06dd0a0f4
malborno2020 <manuel.albornoz@zoho.com>
2021-10-21 20:39:12 -0300

    agregando validaciones a modelo restaurante

--- app/models/puntaje.rb
+++ app/models/puntaje.rb
@@ -1,3 +1,4 @@
 class Puntaje < ApplicationRecord
     validates   :tipo, presence:    true
+    validates   :tipo, uniqueness:  true
 end
--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -1,3 +1,6 @@
 class Restaurante < ApplicationRecord
   belongs_to :tipo_comida
+
+  validates :nombre, presence :true
+  validates :nombre, uniqueness :true  
 end
