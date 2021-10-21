823db5a9f81d9ae811b5f7416420a9f06dfe2407
malborno2020 <manuel.albornoz@zoho.com>
2021-10-20 21:05:00 -0300

    agregando modelo puntaje controladores y vistas

--- app/controllers/puntajes_controller.rb
+++ app/controllers/puntajes_controller.rb
@@ -1,19 +1,59 @@
 class PuntajesController < ApplicationController
 
+    #GET/puntajes/listar
+    def listar
+        @todos_los_puntajes = Puntaje.all.order(id: :asc)
+        @titulo_link = "Registrar un nuevo puntaje"
+    end
+
     #GET/puntajes/nuevo
-def crear
-    @puntaje = Puntaje.new
-end
+    def crear
+        @puntaje = Puntaje.new
+    end
 
-#GET/puntajes/listar
-def listar
-    @todos_los_puntajes = Puntajes.all.order(id: :asc)
-    @titulo_link = "Registrar un nuevo puntaje"
-end
+    # GET /puntajes/:id
+    def mostrar
+        # vista para mostrar el detalle de un puntaje
+        # por ejemplo, mostrar todos los restaurantes con ese puntaje
+        @puntaje = Puntaje.find(params[:id])
+    end
 
-# GET /puntajes/:id
-def mostrar
-    # vista para mostrar el detalle de un puntaje
-    # por ejemplo, mostrar todos los restaurantes con ese puntaje
-    @puntaje = Puntaje.find(params[:id])
+    # GET /puntajes/:id/editar
+    def editar
+        # mostrar el formulario con los datos de un registro para cambiarlos
+        @puntaje = Puntaje.find(params[:id])
+    end
+
+    # POST /puntajes
+    def guardar
+        # guardar lo que llegue del formulario en la base de datos
+        datos_puntaje = params.require(:puntaje).permit(:tipo)
+        nuevo_puntaje = Puntaje.new(datos_puntaje)
+        nuevo_puntaje.save
+
+        redirect_to puntajes_path
+    end
+
+    def actualizar
+        # encontrar el registro que quiero editar en la BD
+        @puntaje = Puntaje.find(params[:id])
+        datos_puntaje = params.require(:puntaje).permit(:tipo)
+        # actualizar los campos necesarios
+        @puntaje.tipo = datos_puntaje[:tipo]
+        # guardar los cambios en la base de datos
+        @puntaje.save
+        # redireccionar a la lista de todos los tipos de comida
+        redirect_to puntajes_path
+    end
+
+    # DELETE /puntajes/:id
+    def eliminar
+        # pasos para eliminar un registro
+        # 1. buscar el registro por ID
+        tipo = Puntaje.find(params[:id])
+        # 2. Intentar eliminar el registro
+        tipo.destroy
+        redirect_to puntajes_path
+    end
+    
 end
