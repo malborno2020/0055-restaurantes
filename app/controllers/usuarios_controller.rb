ed6714e55ec88626154f3c5e8f3cadcf8a5635d1
malborno2020 <manuel.albornoz@zoho.com>
2021-10-22 20:04:59 -0300

    agregando metodo para agregar 1 usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -3,4 +3,19 @@ class UsuariosController < ApplicationController
     def crear
         @usuario = Usuario.new
     end
+
+    #POST/usuarios
+    def guardar
+        #controlar que variables recibir
+        datos_usuario = params.require(:usuario).permit(:nombre_usuario, :password, :password_confirmation)
+        #cuidado de que esta variables quede igual que la de la vista
+        @usuario = Usuario.new(datos_usuario)
+        if @usuario.save
+            #mostrar un mensaje de confirmacion
+        else
+            render  :crear
+        end
+
+
+    end
 end
